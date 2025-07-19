a63271cecc0a376354808fd910729b4b905a9f7f
Kaz <kazerog.workspace@gmail.com>
2025-07-19 21:51:28 +0300

    ~compose

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 
-const BACKEND_URL = 'http://localhost:8000'
+const BACKEND_URL = 'http://backend:8000'
 
 export async function GET(
   request: Request,
